6df7b319f617abe89ada8ce05398b91e61902407
Yonil <201913301c@utea.edu.pe>
2022-10-03 12:15:07 -0500

    ingreso de nombre

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,21 @@ class MyApp extends StatelessWidget {
               backgroundColor: Colors.white,
               backgroundImage: NetworkImage("https://images.pexels.com/photos/1278566/pexels-photo-1278566.jpeg"),
             ),
+
+            Text("Kratos' de Loki",
+            style: TextStyle(
+             color: Colors.white,
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+
+            ),),
+            Text("FLUTTER DEVELOPER",
+            style: TextStyle(
+              color: Colors.white38,
+              fontSize: 18.0,
+              fontWeight: FontWeight.w300,
+              letterSpacing: 2.0,
+            ),),
           ],
         ),
 
