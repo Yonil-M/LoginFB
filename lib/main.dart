a463d43ae07b6e4f7c3414ccf1939cd62b0d230f
Yonil <201913301c@utea.edu.pe>
2022-10-03 18:06:01 -0500

    Iconos fb y mas

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,10 @@ class MyApp extends StatelessWidget {
             CircleAvatar(
               radius: 60.0,
               backgroundColor: Colors.white,
-              backgroundImage: AssetImage("images\persona1.jpeg"),
+              backgroundImage: AssetImage("assets/images/persona2.jpeg"),
             ),
 
-            Text("Kratos' de Loki",
+            Text("Fiolella De La Cruz Martinez",
             style: TextStyle(
              color: Colors.white,
               fontSize: 25.0,
@@ -40,35 +40,65 @@ class MyApp extends StatelessWidget {
               
               thickness: 0.70,
               color: Colors.white38,
-              indent: 80.0,
-              endIndent: 120.0,
+              indent: 90.0,
+              endIndent: 85.0,
             ),
 
             Card(
               color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 15.0,),
+              margin: EdgeInsets.symmetric(vertical: 8.0,horizontal: 15.0),
               child: ListTile(
                 title: Text("+51 456789123"),
                 subtitle: Text("Telefono"),
-                leading: Icon(Icons.phone,
+                leading: Icon(Icons.phone_outlined,
                 color:Colors.indigo ),
-                trailing: Icon(Icons.check_circle,
+                trailing: Icon(Icons.check_circle_outline_outlined,
                 color:Colors.indigo),
               ),
             ),
 
             Card(
               color: Colors.white,
-              margin: EdgeInsets.all(15.0),
+              margin: EdgeInsets.symmetric(vertical: 8.0,horizontal: 15.0),
               child: ListTile(
                 title: Text("Yonil_@gmail.com"),
                 subtitle: Text("Correo"),
-                leading: Icon(Icons.mail,
+                leading: Icon(Icons.mail_outline,
                 color:Colors.indigo ),
-                trailing: Icon(Icons.check_circle,
+                trailing: Icon(Icons.check_circle_outline,
                 color:Colors.indigo),
               ),
             ),
+            SizedBox(height: 8.0,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 50.0,
+                  width: 50.0,
+                  color: Colors.indigo,
+                  child: Image.network("https://serviciosimpecable.com/wp-content/uploads/2020/07/fb-1.png",
+                  fit: BoxFit.cover  ,),
+                ),
+                SizedBox(width: 4.0,),
+                Container(
+                  height: 50.0,
+                  width: 50.0,
+                  color: Colors.indigo,
+                  child: Image.network("https://us.123rf.com/450wm/alsstocks450/alsstocks4501911/alsstocks450191100175/136346991-voronezh-rusia-21-de-noviembre-de-2019-icono-cuadrado-del-logotipo-de-twitter-en-color-azul.jpg",
+                  fit: BoxFit.fitHeight ,),
+                ),
+                SizedBox(width: 4.0,),
+                Container(
+                  height: 50.0,
+                  width: 50.0,
+                  color: Colors.indigo,
+                  child: Image.network("https://w7.pngwing.com/pngs/367/971/png-transparent-brand-product-design-logo-facebook-and-instagram-logo-blue-child-text.png",
+                  fit: BoxFit.cover  ,),
+                )
+              ],
+            )
+            
           ],
         ),
 
