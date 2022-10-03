dc35140d1456dd44f4ee743b18e8c84e3d080345
Yonil <201913301c@utea.edu.pe>
2022-10-03 15:56:32 -0500

    card

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
             CircleAvatar(
               radius: 60.0,
               backgroundColor: Colors.white,
-              backgroundImage: NetworkImage("https://images.pexels.com/photos/1278566/pexels-photo-1278566.jpeg"),
+              backgroundImage: AssetImage("images\persona1.jpeg"),
             ),
 
             Text("Kratos' de Loki",
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
              color: Colors.white,
               fontSize: 25.0,
               fontWeight: FontWeight.bold,
+              fontFamily: "Dancing",
 
             ),),
             Text("FLUTTER DEVELOPER",
@@ -35,6 +36,39 @@ class MyApp extends StatelessWidget {
               fontWeight: FontWeight.w300,
               letterSpacing: 2.0,
             ),),
+            Divider(
+              
+              thickness: 0.70,
+              color: Colors.white38,
+              indent: 80.0,
+              endIndent: 120.0,
+            ),
+
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 15.0,),
+              child: ListTile(
+                title: Text("+51 456789123"),
+                subtitle: Text("Telefono"),
+                leading: Icon(Icons.phone,
+                color:Colors.indigo ),
+                trailing: Icon(Icons.check_circle,
+                color:Colors.indigo),
+              ),
+            ),
+
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.all(15.0),
+              child: ListTile(
+                title: Text("Yonil_@gmail.com"),
+                subtitle: Text("Correo"),
+                leading: Icon(Icons.mail,
+                color:Colors.indigo ),
+                trailing: Icon(Icons.check_circle,
+                color:Colors.indigo),
+              ),
+            ),
           ],
         ),
 
